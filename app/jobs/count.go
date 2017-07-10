3678155c81a62aab113dd5e4579c2fc9d750fe7e
gunendu <gunnedu@gmail.com>
2017-07-10 14:27:26 +0530

    first test commit

---
+++ app/jobs/count.go
@@ -0,0 +1,28 @@
+package jobs
+
+import (
+	"fmt"
+	"github.com/revel/revel"
+    
+	"github.com/revel/modules/jobs/app/jobs"
+	"github.com/gunendu/calorieCounter/app/controllers"
+	"github.com/gunendu/calorieCounter/app/models"
+)
+
+// Periodically count the bookings in the database.
+type BookingCounter struct{}
+
+func (c BookingCounter) Run() {
+	bookings, err := controllers.Dbm.Select(models.Booking{},
+		`select * from Booking`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("There are %d bookings.\n", len(bookings))
+}
+
+func init() {
+	revel.OnAppStart(func() {
+		jobs.Schedule("@every 1m", BookingCounter{})
+	})
+}
